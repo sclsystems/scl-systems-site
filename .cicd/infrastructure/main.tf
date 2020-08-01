fb09fe25b89d03c184ec09c060de279f1487cad3
James Marino <james@marino.io>
2020-08-01 20:40:12 +1000

    feat: added multisite

--- .cicd/infrastructure/main.tf
+++ .cicd/infrastructure/main.tf
@@ -1,10 +1,11 @@
-module "static_site_infrastruture" {
-  source = "git::https://github.com/sclsystems/static-site-infrastructure.git?ref=631efb848120b65697ab141ba6b99cf38556ef4d"
+module "static_site_infrastructure" {
+  source = "git::https://github.com/sclsystems/static-site-infrastructure.git?ref=384b1c1b2ce33e6dbf0397b528910219ddce99f0"
 
+  aws_region              = var.aws_region
   domain_name             = var.domain_name
+  mirror_domains          = var.mirror_domains
   project_name            = var.project_name
   git_repository          = var.git_repository
   buildspec_location      = var.buildspec_location
-  domain_hosted_zone_id   = var.domain_hosted_zone_id
   cloudfront_refer_secret = var.cloudfront_refer_secret
 }
